7035640bc0bd1ac5c4885b18655ce037f6cfac3b
FORSAA <EgorZaharov51@yandex.ru>
2024-11-30 21:22:02 +0300

    upd

--- O_D_L_store/static/main/js/index.js
+++ O_D_L_store/static/main/js/index.js
@@ -21,3 +21,19 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     });
 });
+
+
+function toggleFilter(filterId) {
+    const filter = document.getElementById(filterId);
+    filter.classList.toggle('hidden');
+}
+
+function applyFilters() {
+    const colors = Array.from(document.querySelectorAll('input[name="color"]:checked')).map(input => input.value);
+    const shapes = Array.from(document.querySelectorAll('input[name="shape"]:checked')).map(input => input.value);
+    const search = document.getElementById('search-box').value;
+    const sort = document.getElementById('sort-options').value;
+    console.log('Фильтры:', { colors, shapes, search, sort });
+
+    const results = document.getElementById('catalog-results');
+}
